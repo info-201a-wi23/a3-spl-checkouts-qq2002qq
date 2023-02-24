a06fd13ac0f8df48a08b165cf6224b6b580fcf7c
qq2002qq <qq2002qq@uw.edu>
2023-02-24 00:05:22 -0800

    fixed error

--- chart1.R
+++ chart1.R
@@ -1,6 +1,21 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
-
 library(ggplot2)
-ggplot() +
-  geom_line(aes(x=x_values, y = y_values))
+library(dplyr)
+
+# load the csv file
+df <- read.csv("2017-2023-10-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
+
+# explore the trend between the material type and year
+df.1 <- df %>%
+  filter(MaterialType == c("BOOK", "EBOOK", "AUDIOBOOK", "VIDEODISC"))
+df.1$CheckoutYear <- as.character(df.1$CheckoutYear)
+df.1$count <- 1
+
+
+
+ggplot(df.1, aes(x = CheckoutYear, y = count, fill = MaterialType)) +
+  geom_col(position = position_stack()) +
+  labs(x = "Year",
+       y = "Amount of Material Types",
+       title = "The changes of usage of different reading materials",
+       fill = "Material Types")
+
--- chart2.R
+++ chart2.R
@@ -1,6 +1,35 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
-
 library(ggplot2)
-ggplot() +
-  geom_line(aes(x=x_values, y = y_values))
+library(dplyr)
+library("scales")
+
+# load the csv file
+df <- read.csv("2017-2023-10-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
+
+df.2 <- df %>%
+  mutate(date = paste0(CheckoutYear, "-", CheckoutMonth, "-01"))
+df.2$date <- as.Date(df.2$date)
+
+df.3 <- df.2 %>%
+  select(date, Checkouts) %>%
+  group_by(date) %>%
+  summarize(total = sum(Checkouts, na.rm = T))
+
+df.4 <- df.2 %>%
+  filter(CheckoutYear == PublicationYear) %>%
+  select(date, Checkouts) %>%
+  group_by(date) %>%
+  summarize(total.2 = sum(Checkouts, na.rm = T))
+
+df.5 <- merge(df.4, df.3)
+
+ggplot(df.5) +
+  geom_line(aes(x = date, y = total), color = "blue") +
+  geom_line(aes(x = date, y = total.2), color = "red") +
+  labs(
+    x = "Date",
+    y = "Total amount of checkouts (in thousand)",
+    title = "Time Series of checkouts",
+    subtitle = "Blue line: traces total checkouts 
+Red line: traces total checkouts that have the same year as publish") +
+  scale_y_continuous(labels = label_number(suffix = " K", scale = 1e-3)) +
+  theme_classic()
