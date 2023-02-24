69c1a32f04901b1ab1868e2a04c99667921a6eb7
qq2002qq <qq2002qq@uw.edu>
2023-02-23 23:50:26 -0800

    csv delete

---
+++ chart3.R
@@ -0,0 +1,18 @@
+library(ggplot2)
+library(dplyr)
+
+# load the csv file
+df <- read.csv("2017-2023-10-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
+
+df.3 <- df %>%
+  group_by(CheckoutType) %>%
+  summarize(number = n()) %>%
+  mutate(prop = round((number * 100 / sum(number)),digits = 2))
+
+ggplot(df.3, aes(x = "", y = prop, fill = CheckoutType)) +
+  geom_bar(width = 1, stat = "identity", color = "white") +
+  coord_polar("y", start = 0)+
+  geom_text(aes(label = ""), color = "white")+
+  theme_void() +
+  labs(title = "Popular Checkout Type Proportion",
+       fill = "Vendors")
