1045bfa65bee2b3f11e2be31b080d35de4075565
qq2002qq <qq2002qq@uw.edu>
2023-03-04 23:58:56 -0800

    chart 2&3 revised

--- chart2.R
+++ chart2.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 library(dplyr)
 library("scales")
+library(plotly)
 
 # load the csv file
 df <- read.csv("2017-2023-10-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
@@ -10,27 +11,17 @@ df.2 <- df %>%
 df.2$date <- as.Date(df.2$date)
 
 df.3 <- df.2 %>%
-  select(date, Checkouts) %>%
-  group_by(date) %>%
-  summarize(total = sum(Checkouts, na.rm = T))
+  select(date, Checkouts, UsageClass) %>%
+  group_by(date,UsageClass) %>%
+  summarize(checkouts = sum(Checkouts, na.rm = T))
 
-df.4 <- df.2 %>%
-  filter(CheckoutYear == PublicationYear) %>%
-  select(date, Checkouts) %>%
-  group_by(date) %>%
-  summarize(total.2 = sum(Checkouts, na.rm = T))
-
-df.5 <- merge(df.4, df.3)
-
-ggplot(df.5) +
-  geom_line(aes(x = date, y = total), color = "blue") +
-  geom_line(aes(x = date, y = total.2), color = "red") +
-  labs(
-    x = "Date",
-    y = "Total amount of checkouts (in thousand)",
-    title = "Time Series of checkouts",
-    subtitle = "Blue line: traces total checkouts 
-Red line: traces total checkouts that have the same year as publish") +
+checkouts.plot <- ggplot(df.3, aes(x = date, y = checkouts, color = UsageClass)) +
+  geom_line()+
+  labs(x = "Date",
+       y = "Total amount of checkouts (in thousand)",
+       title = "Time Series of checkouts",
+       color = "Usage Class") +
   scale_y_continuous(labels = label_number(suffix = " K", scale = 1e-3)) +
   theme_classic()
 
+ggplotly(checkouts.plot)
--- chart3.R
+++ chart3.R
@@ -1,25 +1,29 @@
 library(ggplot2)
 library(dplyr)
+library(plotly)
 
 # load the csv file
 df <- read.csv("2017-2023-10-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
 
-df.3 <- df %>%
-  group_by(CheckoutType) %>%
-  summarize(number = n()) %>%
-  mutate(prop = round((number * 100 / sum(number)),digits = 2))
+df.2 <- df %>%
+  mutate(date = paste0(CheckoutYear, "-", CheckoutMonth, "-01"))
+df.2$date <- as.Date(df.2$date)
 
-ggplot(df.3, aes(x = "", y = prop, fill = CheckoutType)) +
-  geom_bar(width = 1, stat = "identity", color = "white") +
-  coord_polar("y", start = 0)+
-  geom_text(aes(label = ""), color = "white")+
-  theme_void() +
-  labs(title = "Popular Checkout Type Proportion",
-       fill = "Vendors",
-       subtitle = "Horizon takes 56.6%,
-OverDrive takes 42.71%,
-Zinio takes 0.32%,
-Hoopla takes 0.26%,
-Freegal take 0.1%")
+df.3 <- df.2 %>%
+  group_by(date, CheckoutType) %>%
+  summarize(number = n())
 
-knitr::kable(df.3)
+plot.3 <- ggplot(df.3, aes(x = CheckoutType, y = number, fill = CheckoutType )) +
+  geom_boxplot() +
+  labs (x = "Checkout Vendor",
+        y = "Number of Vendor",
+        title = "Boxplot of Vendors",
+        fill = "Vendors")
+
+ggplotly(plot.3)
+
+
+
+
+
+
